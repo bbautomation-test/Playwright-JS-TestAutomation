c779da787047060f0a0420d9722ef2b242fc8a1d
bbautomation-test <heybiswahere@gmail.com>
2025-12-10 10:29:19 +0530

    Added new practice code

--- tests/ClientAPP.spec.js
+++ tests/ClientAPP.spec.js
@@ -14,7 +14,7 @@ test('Wait dynamically for service based application', async ({ page }) => {
     console.log(await cardTiles.allTextContents());
 });
 //Section5 17- Handling static dropdown and radio btn
-test.only('Page global fixture', async ({ page }) => {
+test('Page global fixture', async ({ page }) => {
     const userName = page.locator("#username");
     const passWord = page.locator("[type='password']");
     const signInCTA = page.locator("#signInBtn");
@@ -23,25 +23,41 @@ test.only('Page global fixture', async ({ page }) => {
     const userRadioBtn = page.locator(".radiotextsty").last();
     const okayPopupBtn = page.locator("#okayBtn");
     const termCheckBox= page.locator("#terms");
+    const documentLink= page.locator("[href*='documents-request']");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await userName.fill("rahulshettyacademy");
     await passWord.fill("learning");
     await dropdown.selectOption("consult");
-    await page.pause();
+    //await page.pause();
     await userRadioBtn.click();
-    await page.pause(); // to pause the script
+   // await page.pause(); // to pause the script
     await okayPopupBtn.click();
     await signInCTA.click();
-    //sectio5 18-assertions for selected radio btns and checkbox
+    //section 18,19,20-assertions for selecting radio btns and checkbox
     await expect(userRadioBtn).toBeChecked();
     console.log(await userRadioBtn.isChecked());
     await termCheckBox.check();
     await expect(termCheckBox).toBeChecked();
     await termCheckBox.uncheck();
-    await expect(termCheckBox).not.
-    
-    toBeChecked();
+    await expect(termCheckBox).not.toBeChecked();
     expect(await termCheckBox.isChecked()).toBeFalsy();
+    await expect(documentLink).toHaveAttribute("class","blinkingText");
 
-       
 });
+//section 21-Handling child windows and tabs by switching broweser context.
+test.only('Handling child windows and tabs', async ({ browser }) => 
+    {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+      const documentLink= page.locator("[href*='documents-request']");
+      const[newpage]=await Promise.all([
+        context.waitForEvent('page'), // listen for any new page event pending, rejected or fullfilled
+        documentLink.click()
+      ]); // new page is opened after clicking on document link
+      const text= await newpage.locator(".red").textContent();
+        console.log(text);
+        const arraytext=text.split("@");
+        const domain=arraytext[1].split(" ")[0];
+        console.log(domain); 
+    });
