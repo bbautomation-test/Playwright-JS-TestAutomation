d6d75f6770d85a0643950b21fbdfe9e49a31a07f
bbautomation-test <heybiswahere@gmail.com>
2026-02-02 15:55:30 +0530

    Fixing as per new chnages in the page

--- tests/ClientAPP.spec.js
+++ tests/ClientAPP.spec.js
@@ -1,4 +1,52 @@
 const { test, expect } = require('@playwright/test');
+test('Browser global fixture', async function ({ browser }) {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+});
+
+test.only('Page global fixture', async ({ page }) => {
+  const userName = page.locator("#username");
+  const passWord = page.locator("[type='password']");
+  const signInCTA = page.locator("#signInBtn");
+  const cardTiles = page.locator(".card-body a");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  //Assertions
+  await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+
+  //section4 11-Locator 
+  await userName.fill("rahulshetty");
+  await passWord.fill("Learning");
+  await signInCTA.click();
+
+  //section4 12 -extracting text fro brwoser
+  // console.log(await page.locator("[style*='block']").textContent());
+  // await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+
+  //section4 13-extract multiple webelements from one locator- traverse
+  await userName.fill(""); //aditional feature of fill (To wipeout the existing data)
+  await userName.fill("rahulshettyacademy");
+  await passWord.fill("learning");
+   const incorrectPasswordText = await page.locator("[style*='block']").textContent();
+  console.log(incorrectPasswordText);
+  const splitContent= incorrectPasswordText.split('"');
+  const newPassword= splitContent[3];
+   console.log(newPassword); 
+  await passWord.fill("");
+  await passWord.fill(newPassword);
+  await signInCTA.click();
+  console.log(await cardTiles.first().textContent()); // traverse from parent to child we need to give a space
+  console.log(await cardTiles.nth(0).textContent()); //textContent() supports autowaiting
+  console.log(await cardTiles.nth(1).textContent());
+  console.log(await cardTiles.last().textContent());
+  await page.waitForLoadState('networkidle'); // we can use this synchronization method or wait mechanism for those method which doesn't support autowaiting.
+  await cardTiles.first().waitFor();// sometimes the above methods may not work so we can user this to wait for particular element to be loaded.
+  console.log(await cardTiles.allTextContents()); // it will return a list even if it contains zero element an autowaiting doesn't work for it
+
+});
+
 test('Wait dynamically for service based application', async ({ page }) => {
     //Section4 15- techniques to wait dynamically for a new page.
     const userName = page.locator("#userEmail");
@@ -14,7 +62,7 @@ test('Wait dynamically for service based application', async ({ page }) => {
     console.log(await cardTiles.allTextContents());
 });
 //Section5 17- Handling static dropdown and radio btn
-test('Page global fixture', async ({ page }) => {
+test('Page global fixtures', async ({ page }) => {
     const userName = page.locator("#username");
     const passWord = page.locator("[type='password']");
     const signInCTA = page.locator("#signInBtn");
@@ -33,7 +81,7 @@ test('Page global fixture', async ({ page }) => {
    // await page.pause(); // to pause the script
     await okayPopupBtn.click();
     await signInCTA.click();
-    //section 18,19,20-assertions for selecting radio btns and checkbox
+    //section 18,19,20, 21-assertions for selecting radio btns and checkbox
     await expect(userRadioBtn).toBeChecked();
     console.log(await userRadioBtn.isChecked());
     await termCheckBox.check();
@@ -44,12 +92,12 @@ test('Page global fixture', async ({ page }) => {
     await expect(documentLink).toHaveAttribute("class","blinkingText");
 
 });
-//section 21-Handling child windows and tabs by switching broweser context.
+//section 23-Handling child windows and tabs by switching broweser context.
 test.only('Handling child windows and tabs', async ({ browser }) => 
     {
     const context = await browser.newContext();
     const page = await context.newPage();
-    page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
       const documentLink= page.locator("[href*='documents-request']");
       const[newpage]=await Promise.all([
         context.waitForEvent('page'), // listen for any new page event pending, rejected or fullfilled
